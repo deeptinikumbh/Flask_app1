653544bcda742c33e940afe6a717b31c774b1805
deeptinikumbh <36128502+deeptinikumbh@users.noreply.github.com>
2025-10-30 12:06:27 +0530

    commit 2

--- mini_api.py
+++ mini_api.py
@@ -9,7 +9,7 @@ model = pickle.load(model)
 #home page
 @app.route("/")
 def hello_world():
-    return {"Message":"Hello Welcome to loan prediction system version 1!!!"}
+    return {"Message":"Hello Welcome to loan prediction system version 2!!!"}
 
 @app.route("/panda")
 def hello_panda():
